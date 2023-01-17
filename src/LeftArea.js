7779dff843a9cc172d375e7c10a9c8a97b201704
ryan3780 <guemhong3780@gmail.com>
2023-01-17 18:44:03 +0900

    remove scorll

--- src/LeftArea.js
+++ src/LeftArea.js
@@ -64,39 +64,24 @@ const LeftArea = () =>{
         menuSelectBarRef.current.style.width = menuSelectRef.current[idx].offsetWidth + 'px'        
     }
 
-    const [startX, setStartX] = useState();
-    const [isDrag, setIsDrag] = useState(false);
-
     const swipeRef = useRef();
-    
-    console.log(swipeRef)
 
-    const handleDragStart = (e) => {
-        console.log(e.clientX)
-        setIsDrag(true);
-        setStartX(e.pageX)
+    let x, left, down;
 
+
+    const onMouseDown = (e) =>{
+        e.preventDefault();
+        down = true;
+        x = e.pageX;
+        left = swipeRef.current.scrollLeft()
     }
 
-    const handleDragMove = (e) => {
-        if(isDrag){
-            console.log(startX, e.clientX)
-            console.log(e.clientX - startX)
-
-            setStartX(e.clientX - startX)
+    const onMouseMove = (e ) =>{
+        if(down){
+            let newX = e.pageX
+            swipeRef.current.scrollLeft (left - newX, x)
         }
-        
     }
-
-    const handleDragEnd = () => {
-        console.log(startX)
-        setIsDrag(false)
-
-    }
-
-    console.log(startX)
-
-// 0 ~ -500px scroll
 
     return (
         <div className="pf-responsive-container" onClick={handleMakeIsClickedFalse}>
@@ -119,11 +104,8 @@ const LeftArea = () =>{
                 <div className='swipe-area'>
                     <ul className='swipe' 
                     ref = {swipeRef}
-                    onMouseDown={handleDragStart}
-                    onMouseMove={ isDrag ? handleDragMove : null}
-                    onMouseUp={handleDragEnd}
-                    onMouseLeave={handleDragEnd}
-                    style={{ transform : !isDrag ? `translateX(${startX}px)` : '' }}>
+                    onMouseDown = {onMouseDown}
+                    >
                         <li className='bar' ref={menuSelectBarRef} style={{width : '41px'}}>Bar</li>
                         {tabMenu.map((item, idx) => {
                             return (<li key={idx}>
